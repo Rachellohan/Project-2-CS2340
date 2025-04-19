77bdeb2c7f8577c2fe185e327232bb6f4cab92ec
M322-tech-debug <mwangimartha82@gmail.com>
2025-04-19 14:36:56 -0400

    Finished changing names

--- pokeTrade/cart/models.py
+++ pokeTrade/cart/models.py
@@ -1,6 +1,6 @@
 from django.db import models
 from django.contrib.auth.models import User
-from movies.models import Movie
+from pokemon.models import Pokemon
 
 class Order(models.Model):
     id = models.AutoField(primary_key=True)
@@ -16,7 +16,7 @@ class Item(models.Model):
     price = models.IntegerField()
     quantity = models.IntegerField()
     order = models.ForeignKey(Order, on_delete=models.CASCADE)
-    movie = models.ForeignKey(Movie, on_delete=models.CASCADE)
+    movie = models.ForeignKey(Pokemon, on_delete=models.CASCADE)
 
     def __str__(self):
         return str(self.id) + ' - ' + self.movie.name
