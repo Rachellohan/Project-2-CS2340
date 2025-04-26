bae188e458cd6786c4cfdac2756a3e2b7fc26706
M322-tech-debug <mwangimartha82@gmail.com>
2025-04-26 16:35:54 -0400

    Actual money changes

--- PokeTradeV1/pokeTrade/pokemons/models.py
+++ PokeTradeV1/pokeTrade/pokemons/models.py
@@ -1,10 +1,11 @@
+import random
 from django.db import models
 from django.contrib.auth.models import User
 
 class Pokemon(models.Model):
     id = models.AutoField(primary_key=True)
     name = models.CharField(max_length=255)
-    price = models.IntegerField()
+    price = models.IntegerField(default=0)
     description = models.TextField()
     image = models.ImageField(upload_to='pokemon_images/')
     owner = models.ForeignKey(User, on_delete=models.CASCADE, default="5")  
@@ -12,6 +13,13 @@ class Pokemon(models.Model):
 
     def __str__(self):
         return str(self.id) + ' - ' + self.name
+    
+    def save(self, *args, **kwargs):
+         # Assign a random money value only if it's not already set
+         if self.price == 0:
+             self.price = random.randint(1, 100)  # Random between 100 and 1000
+         super().save(*args, **kwargs)
+ 
 
 class Review(models.Model):
     id = models.AutoField(primary_key=True)
