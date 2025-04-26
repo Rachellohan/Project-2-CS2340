37554e5b4c02ca168668ed3cccbdbd6c30f593fd
M322-tech-debug <mwangimartha82@gmail.com>
2025-04-26 16:35:54 -0400

    Actual money changes

--- PokeTradeV1/pokeTrade/pokemons/models.py
+++ PokeTradeV1/pokeTrade/pokemons/models.py
@@ -1,4 +1,5 @@
 import random
+import random
 from django.db import models
 from django.contrib.auth.models import User
 
@@ -19,8 +20,9 @@ class Pokemon(models.Model):
         return str(self.id) + ' - ' + self.name
     
     def save(self, *args, **kwargs):
+         # Assign a random money value only if it's not already set
          if self.price == 0:
-             self.price = random.randint(1, 20)
+             self.price = random.randint(1, 100)  # Random between 100 and 1000
          super().save(*args, **kwargs)
  
 
