51737a304c7224d0f495f0ef78e0e43e9fbc3e8c
M322-tech-debug <mwangimartha82@gmail.com>
2025-04-26 00:54:32 -0400

    adding views

--- PokeTradeV1/pokeTrade/friends_page/views.py
+++ PokeTradeV1/pokeTrade/friends_page/views.py
@@ -1,5 +1,3 @@
-<<<<<<< HEAD
-<<<<<<< HEAD
 from django.shortcuts import render, redirect
 from .models import Friend, User
 
@@ -16,29 +14,4 @@ def index(request):
         'players': all_players,
         'friends': current_friends,
     }
-=======
-from django.shortcuts import render
-
-def index(request):
-    template_data = {}
-    template_data['title'] = 'Friends'
->>>>>>> d5e5002 (pycache files)
-=======
-from django.shortcuts import render, redirect
-from .models import Friend, User
-
-def index(request):
-    user = request.user
-    if not request.user.is_authenticated:
-        return redirect('accounts.login')
-    all_players = User.objects.exclude(id=user.id)
-
-    current_friends = Friend.objects.filter(user=user).values_list('friend_id', flat=True)
-
-    template_data = {
-        'title': 'Friends',
-        'players': all_players,
-        'friends': current_friends,
-    }
->>>>>>> 629866d (Finishing formatting)
     return render(request, 'friends_page/index.html', {'template_data': template_data})
