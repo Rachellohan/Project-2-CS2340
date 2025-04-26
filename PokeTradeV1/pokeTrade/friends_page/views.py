b81e5657b592ae9adccc38c2de7e7d09830c2570
M322-tech-debug <mwangimartha82@gmail.com>
2025-04-26 00:11:28 -0400

    Finishing formatting

--- PokeTradeV1/pokeTrade/friends_page/views.py
+++ PokeTradeV1/pokeTrade/friends_page/views.py
@@ -1,4 +1,5 @@
 <<<<<<< HEAD
+<<<<<<< HEAD
 from django.shortcuts import render, redirect
 from .models import Friend, User
 
@@ -22,4 +23,22 @@ def index(request):
     template_data = {}
     template_data['title'] = 'Friends'
 >>>>>>> d5e5002 (pycache files)
+=======
+from django.shortcuts import render, redirect
+from .models import Friend, User
+
+def index(request):
+    user = request.user
+    if not request.user.is_authenticated:
+        return redirect('accounts.login')
+    all_players = User.objects.exclude(id=user.id)
+
+    current_friends = Friend.objects.filter(user=user).values_list('friend_id', flat=True)
+
+    template_data = {
+        'title': 'Friends',
+        'players': all_players,
+        'friends': current_friends,
+    }
+>>>>>>> 629866d (Finishing formatting)
     return render(request, 'friends_page/index.html', {'template_data': template_data})
