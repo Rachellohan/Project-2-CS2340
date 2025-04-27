2b25e742ca573766b53c2ab618699d2212b0a036
hgarlapati3 <hgarlapati3@gatech.edu>
2025-04-26 22:28:21 -0400

    configuring trading

--- PokeTradeV1/pokeTrade/accounts/models.py
+++ PokeTradeV1/pokeTrade/accounts/models.py
@@ -1,3 +1,7 @@
+from django.contrib.auth.models import User
 from django.db import models
 
-# Create your models here.
+class Pokemon(models.Model):
+    name = models.CharField(max_length=100)
+    image = models.URLField()
+    owner = models.ForeignKey(User, on_delete=models.CASCADE, related_name='pokemons', null=True, blank=True)
--- PokeTradeV1/pokeTrade/accounts/urls.py
+++ PokeTradeV1/pokeTrade/accounts/urls.py
@@ -7,4 +7,5 @@ urlpatterns = [
     path('logout/', views.logout, name='accounts.logout'),
     path('orders/', views.orders, name='accounts.orders'),
     path('profile/', views.profile, name='accounts.profile'),
+    path('trade/<int:pokemon_id>/', views.trade, name='accounts.trade'),
 ]
